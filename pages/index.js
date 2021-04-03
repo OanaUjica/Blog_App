b613c8fd5d2b3aacd3fc8d9cfb3f308e0091b811
Oana Ujica <48068668+OanaUjica@users.noreply.github.com>
2021-04-03 23:39:33 +0300

    Testing routing integration

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,6 +13,13 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://github.com/OanaUjica" target="_blank">Oana's portfolio</a>
         </h1>
+
+        <p className="title">
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
+        </p>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
